32ccb5b212dea821e2fc888a15bc2b5b6327f5d0
Vedic Dutta <109514492+JLiebert73@users.noreply.github.com>
2024-01-05 02:30:51 +0530

    Added comment and like functionality

--- blog/test/models.py
+++ blog/test/models.py
@@ -3,6 +3,17 @@ from test import db, login_manager
 from flask_login import UserMixin
 
 
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Comment('{self.content}', {self.date_posted}')"
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -14,17 +25,21 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-class Post(db.Model, UserMixin):
+
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(20), nullable=True, default="Flask Blogpost")
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
-    
+    comments = db.relationship('Comment', backref='post', lazy=True)
+    likes = db.Column(db.Integer, default=0)  # New column for likes
 
     def __repr__(self):
         return f"Post('{self.title}', {self.date_posted}')"
+
